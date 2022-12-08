2153a409df389a2ded40d6ea455a297c161f3591
suyash mhatre <55102993+suyash0702@users.noreply.github.com>
2022-12-09 00:16:54 +0530

    Add files via upload

---
+++ basket_sc/index.js
@@ -0,0 +1,36 @@
+let h_score = document.getElementById('hScore')
+let count = 0 
+
+function plusone() {
+    count += 1
+    h_score.innerText = count 
+}
+
+function plustwo() {
+    count += 2
+    h_score.innerText = count 
+}
+
+function plusthree() {
+    count += 3
+    h_score.innerText = count 
+}
+
+
+let g_score = document.getElementById('gScore')
+let gcount = 0 
+
+function g_plusone() {
+    gcount += 1
+    g_score.innerText = gcount 
+}
+
+function g_plustwo() {
+    gcount += 2
+    g_score.innerText = gcount 
+}
+
+function g_plusthree() {
+    gcount += 3
+    g_score.innerText = gcount 
+}
